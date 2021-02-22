40a08b0e51453386c81212f3af784cd0483203f0
Sethhhhhhh <yohannvvt@gmail.com>
2021-02-22 15:00:08 +0100

    fix problem with -n in echo command.=

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -8,14 +8,20 @@ int	run_echo(char **args)
 	n = 0;
 	i = 1;
 	if (!args[1])
+	{
 		ft_putchar_fd('\n', 1);
+		return (1);
+	}
 	if ((ft_strequ(args[1], " ") && !args[2]))
-		return (0);
-	if (!strcmp(args[1], "-n") && ++i)
-		n = 1;
+		return (1);
+	while (args[i] && ft_strequ(args[i], "-n"))
+	{
+		n++;
+		i++;
+	}
 	while (args[i])
 	{
-		if ((i > 1 && !n) || (i > 2 && n))
+		if ((i > 1 && !n) || (i > n + 1 && n))
 			ft_putchar_fd(' ', 1);
 		ft_putstr_fd(args[i], 1);
 		i++;
